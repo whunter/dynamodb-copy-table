33defe8c7512bea113d37593b8cb677b3a6851c0
Lee Hunter <whunter@vt.edu>
2025-03-31 15:00:13 -0400

    better comment

--- run-dynamodb-copy-table.sh
+++ run-dynamodb-copy-table.sh
@@ -1,7 +1,7 @@
 src="-<some dynamo hash here>-<src env name>"
 dest="-<some dynamo hash here>-<dest env name>"
 # example:
-# src="ef4g5ek0flipflopjindw27blh-dev"
+# src="-ef4g5ek0flipflopjindw27blh-dev"
 
 for table in Archive Collection Collectionmap Embargo History PageContent Site
  do
